ff62a5655aca057f17c9d2e5348bd2ce15afe350
forsandree <efa@efa.nu>
2015-04-03 14:19:00 +0200

    Update addItemToAreaTasks.js

--- website/javascripts/mock-data/addItemToAreaTasks.js
+++ website/javascripts/mock-data/addItemToAreaTasks.js
@@ -14,5 +14,25 @@ CS.AddItemToAreaTasks = [
         workbookAreaId: 4,
         text: "Have you won any awards or prices for you work or educational achievements?",
         sentenceStart: "I was "
-    }
+    },
+    {
+        id: 4,
+        workbookAreaId: 4,
+        text: "Think about the last job you had. One thing you achieved when you worked there was..."
+    },
+    {
+        id: 5,
+        workbookAreaId: 17,
+        text: "What tracks would you like to persue at some point?"
+    },
+    {
+        id: 6,
+        workbookAreaId: 17,
+        text: "Anythng you dreamed of when you were younger but haven't done and still would find exciting to try out?"
+    },
+    {
+        id: 7,
+        workbookAreaId: 17,
+        text: "If you get a salary for working on anything you want, what would you work with? "
+    } 
 ];
