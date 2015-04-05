6f13aac155ba58ce0043877e6440777eea8da348
Christophe Bram <cbramdit@gmail.com>
2015-04-05 09:03:56 +0200

    Added background to the AddItem2Area task panel

--- website/public/mock-data.js
+++ website/public/mock-data.js
@@ -34,5 +34,93 @@ CS.AddItemToAreaTasks = [
         id: 7,
         workbookAreaId: 17,
         text: "If you get a salary for working on anything you want, what would you work with? "
+    },
+    {
+        id: 8,
+        workbookAreaId: 2,
+        text: "What things are you good at? "
+    },
+    {
+        id: 9,
+        workbookAreaId: 2,
+        text: "What qualities were important in the last role you had"
+    },
+    {
+        id: 10,
+        workbookAreaId: 2,
+        text: "What are you the most confident at in your work? "
+    },
+    {
+        id: 11,
+        workbookAreaId: 2,
+        text: "When are you the happiest with your work? ",
+        sentenceStart: "When I "
+    },
+    {
+        id: 12,
+        workbookAreaId: 1,
+        text: "Why do you go to work, except from the salary? "
+    },
+    {
+        id: 13,
+        workbookAreaId: 1,
+        text: "When do you feel the most energy at work? ",
+        sentenceStart: "When I "
+    },
+    {
+        id: 14,
+        workbookAreaId: 1,
+        text: "What are you passions? ",
+        sentenceStart: "I'm passionate about "
+    },
+    {
+        id: 15,
+        workbookAreaId: 3,
+        text: "What kind of environment are you the most creative in? "
+    },
+    {
+        id: 16,
+        workbookAreaId: 3,
+        text: "What kind of environment are you the most productive in? "
+    },
+    {
+        id: 17,
+        workbookAreaId: 1,
+        text: "What impact do you want to have on the world? "
+    },
+    {
+        id: 18,
+        workbookAreaId: 11,
+        text: "What would you like to spend more time on in your work? "
+    },
+    {
+        id: 19,
+        workbookAreaId: 11,
+        text: "In what area do you want to improve yourself in your work? "
+    },
+    {
+        id: 20,
+        workbookAreaId: 11,
+        text: "What are the tasks you look the most forward to? "
+    },
+    {
+        id: 21,
+        workbookAreaId: 16,
+        text: "What methods do you use to do your work? "
+    },
+    {
+        id: 22,
+        workbookAreaId: 16,
+        text: "What are the tools you consider indespensible to do your work? "
+    },
+    {
+        id: 23,
+        workbookAreaId: 16,
+        text: "If you could freely select the tools and methodologies to do your work, what would you chose? "
+    },
+    {
+        id: 24,
+        workbookAreaId: 8,
+        text: "This is something my boss should keep in mind to make me stay... "
     }
 ];
