98c894d783f8f51837f9f3b2fe0609024e172276
forsandree <efa@efa.nu>
2015-06-03 13:52:24 +0200

    Translations

--- website/javascripts/controllers/workbook-area/task-complete/prioritizeItemsComplete.react.js
+++ website/javascripts/controllers/workbook-area/task-complete/prioritizeItemsComplete.react.js
@@ -2,10 +2,9 @@ CS.Controllers.WorkbookAreaPrioritizeItemsComplete = React.createClass({
     render: function () {
         return (
             <div id="task-complete-pep-talk">
-                <h2><i className="fa fa-star"></i>Awesome!<i className="fa fa-star"></i></h2>
-                <p>Dig deeper into what you’ve discovered.<br />
-                    Confirm, find examples and describe more thoroughly.</p>
-                <p>Click on one of the items below to get started:</p>
+                <h2><i className="fa fa-star"></i>Super!<i className="fa fa-star"></i></h2>
+                <p>Nu ska vi gå in lite djupare på de saker du identifierat. Ge exempel, förklara och bekräfta att de här styrkorna verkligen är viktiga. </p>
+                <p>Klicka på en av punkteran nedan för att komma vidare: </p>
             </div>
             );
     }
