338814ace95bdda9e238d94bb8db9e657e14d046
forsandree <efa@efa.nu>
2015-06-03 13:30:01 +0200

    Translations

--- website/javascripts/controllers/workbook-area/task-complete/customTaskComplete.react.js
+++ website/javascripts/controllers/workbook-area/task-complete/customTaskComplete.react.js
@@ -2,9 +2,9 @@ CS.Controllers.WorkbookAreaCustomTaskComplete = React.createClass({
     render: function () {
         return (
             <div id="task-complete-pep-talk">
-                <h2><i className="fa fa-star"></i>Thanks!<i className="fa fa-star"></i></h2>
-                <p>A career advisor will get back to you shortly.</p>
-                <p>In the meantime, we invite you to continue working on this topic, or maybe switch to another one&#63;</p>
+                <h2><i className="fa fa-star"></i>Tack!<i className="fa fa-star"></i></h2>
+                <p>En karriäragent kommer att ta en titt för att hjälpa dig vidare ytterligare. </p>
+                <p>Jobba gärna vidare under tiden! Med det här ämnet eller med ett annat. </p>
                 <div className="centered-contents">
                     <button className="btn btn-primary" onClick={this._handleTaskCompletePepTalkClosed}>Continue</button>
                 </div>
