4360fdc9bf58ce982c11dbdfcdbe88c90dc90277
forsandree <efa@efa.nu>
2015-06-03 15:54:43 +0200

    Text edit

--- website/javascripts/controllers/workbook-item/task-complete/addItemComplete.react.js
+++ website/javascripts/controllers/workbook-item/task-complete/addItemComplete.react.js
@@ -4,7 +4,7 @@ CS.Controllers.WorkbookItemAddItemComplete = React.createClass({
             <div id="task-complete-pep-talk">
                 <h2><i className="fa fa-star"></i>Bra jobbat! <i className="fa fa-star"></i></h2>
                 <p>Lägg till fler exempel och förklara djupare. Ju längre du går, desto bättre. </p>
-                <p>Du kan också <a onClick={this._navigateBack}>gå tillbaka för att jobba med en annan av dina styrkor.</a></p>
+                <p>Du kan också gå tillbaka för att jobba med <a onClick={this._navigateBack}>en annan av dina styrkor.</a></p>
                 <div className="centered-contents">
                     <button className="btn btn-primary" onClick={this._handleTaskCompletePepTalkClosed}>Ok!</button>
                 </div>
